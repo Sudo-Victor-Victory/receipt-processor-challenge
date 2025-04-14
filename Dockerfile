061c2da85150ef0193de072e3d7fe18c352c9b13
Victor Rodriguez <thevictor795@gmail.com>
2025-04-14 15:53:23 -0700

    Added tests, and validation onto Structs

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,9 @@ RUN go mod init workspace
 # Utilizes Gin - a Golang web framework for fast API Dev.
 RUN go get github.com/gin-gonic/gin
 RUN go get github.com/google/uuid
+RUN go get github.com/stretchr/testify
+RUN go get github.com/stretchr/testify/assert@v1.10.0
 
 COPY main.go .
+COPY main_test.go . 
 
--- main.go
+++ main.go
@@ -2,39 +2,51 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
-	"fmt"
 	"github.com/google/uuid"
+	"github.com/go-playground/validator/v10"
 )
+
+var validate *validator.Validate
+func init() {
+    validate = validator.New()
+}
 
 // Represents the items within a customer's Receipt.
 type Item struct {
-    ShortDescription string  `json:"shortDescription"`
-    Price            string  `json:"price"`
+    ShortDescription string `json:"shortDescription" validate:"required"`
+    Price            string `json:"price" validate:"required"`
 }
 
-// Purchase represents the total purchase transaction
 type Receipt struct {
-    Retailer     	string `json:"retailer"`
-    PurchaseDate 	string `json:"purchaseDate"`
-    PurchaseTime 	string `json:"purchaseTime"`
-    Items        	[]Item `json:"items"`
-    Total        	string `json:"total"`
+    Retailer      string `json:"retailer" validate:"required"`
+    PurchaseDate  string `json:"purchaseDate" validate:"required"`
+    PurchaseTime  string `json:"purchaseTime" validate:"required"`
+    Items         []Item `json:"items" validate:"required,dive"`
+    Total         string `json:"total" validate:"required"`
 }
 
 var receiptsMap = make(map[string]Receipt)
 
 func add_receipt(context *gin.Context){
 	var newReceipt Receipt
+
 	// Attempts to convert JSON within request to our todo type.
 	if err := context.BindJSON(&newReceipt); err != nil{
-		// This is where a descriptive error should go.
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"Error": "Could not transform json into Receipt"})
 		return
 	}
+
+    // Validate the receipt
+	if err := validate.Struct(newReceipt); err != nil {
+        context.IndentedJSON(http.StatusBadRequest, gin.H{"Error": "Validation failed", "details": err.Error()})
+        return
+    }
+
 	// Convert to string otherwise incompatable with Golang's map.
 	newID := uuid.New().String()
+	// Adds ID : Receipt to Map.
 	receiptsMap[newID] = newReceipt
 	context.IndentedJSON(http.StatusCreated, "id:" + newID)
-
 }
 
 func main(){
